44053451356286d82d244408132c0cfb15437149
Caio Vasconcelos <caiovasconcelos01@live.com>
2025-03-29 12:30:41 -0300

    Adiciona a classe Node com métodos para manipulação de dados e referência ao próximo nó

--- Dungeons_Elgrably/src/Node.java
+++ Dungeons_Elgrably/src/Node.java
@@ -1,3 +1,27 @@
 public class Node {
+    private Object data;  // a data é um Object para generalizar o tipo de dado
+    private Node next;
+
+    public Node (Object data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
     
 }
