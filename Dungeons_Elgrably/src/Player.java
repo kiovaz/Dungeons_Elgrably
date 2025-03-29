4469c1210715dc912932abb162afe3b20143449c
Caio Vasconcelos <caiovasconcelos01@live.com>
2025-03-29 12:24:39 -0300

    Atualiza a classe Player para usar String como tipo de senha e adiciona método para criar novos jogadores. Cria classes Node e Queue.

--- Dungeons_Elgrably/src/Player.java
+++ Dungeons_Elgrably/src/Player.java
@@ -1,16 +1,15 @@
 public class Player {
     private int idPlayer;
     private String name;
-    private int password;
+    private String password;
     private int balance;
     // TODO Fila encadeada de Personagens ( cada jogador pode ter varios)
-    
-    
-    public Player () {
+
+    public Player() {
 
     }
-    
-    public Player(int idPlayer, String name, int password, int balance) {
+
+    public Player(int idPlayer, String name, String password, int balance) {
         this.idPlayer = idPlayer;
         this.name = name;
         this.password = password;
@@ -33,11 +32,11 @@ public class Player {
         this.name = name;
     }
 
-    public int getPassword() {
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword(int password) {
+    public void setPassword(String password) {
         this.password = password;
     }
 
@@ -45,17 +44,38 @@ public class Player {
         return balance;
     }
 
-    
-    //TODO METODO PARA CADASTRAR
-    
-    
+    // TODO METODO PARA CADASTRAR
+    public Player newPlayer(int idPlayer, String name, String password, int balance) {
+
+        if (idPlayer <= 0) {
+            System.out.println("error: invalid Player ID.");
+            return null;
+        }
+
+        if (name == null || name.trim().isEmpty()) {
+            System.out.println("error: invalid Name");
+            return null;
+        }
+
+        if (password == null || name.trim().isEmpty()) {
+            System.out.println("error: invalid Password.");
+            return null;
+        }
+
+        if ( balance < 0) {
+            System.out.println("error : invalid balance");
+            return null;
+        }
+
+        Player newPlayer = new Player(idPlayer, name, password, balance);
+        System.out.println("Player" + name + "registered.");
+        return newPlayer;
+    }
+
     // TODO METODO PARA AUTENTICAR()
 
-    
     // TODO METODO PARA CRIAR PERSONAGEM
 
     // TODO METODO PARA SELECIONAR PERSONAGEM
-    
 
-    
 }
