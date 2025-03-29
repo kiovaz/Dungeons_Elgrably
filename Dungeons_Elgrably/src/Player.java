5276ad7f83dc1b749227b9b87c2eb6c4850c024c
Caio Vasconcelos <caiovasconcelos01@live.com>
2025-03-28 22:10:44 -0300

    Inserindo TODO's para a classe Player

--- Dungeons_Elgrably/src/Player.java
+++ Dungeons_Elgrably/src/Player.java
@@ -1,5 +1,61 @@
 public class Player {
-    private int playerId;
+    private int idPlayer;
     private String name;
+    private int password;
+    private int balance;
+    // TODO Fila encadeada de Personagens ( cada jogador pode ter varios)
+    
+    
+    public Player () {
+
+    }
+    
+    public Player(int idPlayer, String name, int password, int balance) {
+        this.idPlayer = idPlayer;
+        this.name = name;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public int getIdPlayer() {
+        return idPlayer;
+    }
+
+    public void setIdPlayer(int idPlayer) {
+        this.idPlayer = idPlayer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    
+    //TODO METODO PARA CADASTRAR
+    
+    
+    // TODO METODO PARA AUTENTICAR()
+
+    
+    // TODO METODO PARA CRIAR PERSONAGEM
+
+    // TODO METODO PARA SELECIONAR PERSONAGEM
+    
+
     
 }
